76d4fd5b90beebbf82ac5533ee3a396af4efe171
刘雨春 <1756303287@qq.com>
2020-08-11 10:33:55 +0800

    1、添加了mineModel

---
+++ ehome/ehome/党建/Mine/MineModel.h
@@ -0,0 +1,17 @@
+//
+//  MineModel.h
+//  ehome
+//
+//  Created by a on 2020/8/11.
+//  Copyright © 2020 刘雨春. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MineModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ehome/ehome/党建/Mine/MineModel.m
@@ -0,0 +1,13 @@
+//
+//  MineModel.m
+//  ehome
+//
+//  Created by a on 2020/8/11.
+//  Copyright © 2020 刘雨春. All rights reserved.
+//
+
+#import "MineModel.h"
+
+@implementation MineModel
+
+@end
