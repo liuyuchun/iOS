f5d1dbc7197e11a794d8f7673c2a43c1e632b3e2
刘雨春 <1756303287@qq.com>
2020-08-11 11:23:31 +0800

    1、新建项目

---
+++ test4/test4/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  test4
+//
+//  Created by a on 2020/8/11.
+//  Copyright © 2020 刘雨春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ test4/test4/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  test4
+//
+//  Created by a on 2020/8/11.
+//  Copyright © 2020 刘雨春. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
